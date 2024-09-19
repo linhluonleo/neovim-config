ebc003496b46b8ba662b8d501b2c02d95c38f651
linhluonleo <linhluonleo04@gmail.com>
2024-09-20 00:48:32 +0700

    update indent width

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,17 +1,17 @@
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    css = { "prettierd" },
-    html = { "prettierd" },
-    python = { "isort", "black" },
-    cpp = { "clang-format" },
-  },
+    formatters_by_ft = {
+        lua = { "stylua" },
+        css = { "prettierd" },
+        html = { "prettierd" },
+        python = { "isort", "black" },
+        cpp = { "clang-format" },
+    },
 
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+    format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_fallback = true,
+    },
 }
 
 return options
